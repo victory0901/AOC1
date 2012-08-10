bed55ab6353b0ffb736a29a66476ec2a563f7a05
victory0901 <jfloyd@fullsail.edu>
2012-08-10 18:02:23 -0400

    Change the background color of the view

--- testApp/testApp/ViewController.m
+++ testApp/testApp/ViewController.m
@@ -16,6 +16,9 @@
 
 - (void)viewDidLoad
 {
+    //Change the background color of the view
+    self.view.backgroundColor = [UIColor grayColor];
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
