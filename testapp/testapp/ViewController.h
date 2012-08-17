e3ed4b928f30366aae184aa8433270313ab83232
victory0901 <jfloyd@fullsail.edu>
2012-08-16 22:50:09 -0400

    Fixed issue of incomplete implementation

--- testapp/testapp/ViewController.h
+++ testapp/testapp/ViewController.h
@@ -14,7 +14,7 @@
 }
 
 - (int)Add:(NSInteger)intOne intTwo:(NSInteger)intTwo;
--(BOOL)Compare:(NSInteger)boolOne BoolTwo:(NSInteger)boolTwo;
+-(BOOL)Compare:(NSInteger)compareOne compareTwo:(NSInteger)compareTwo;
 -(NSString*)Append:(NSString*)stringOne stringTwo:(NSString*)stringTwo;
 -(void)DisplayAlertWithString:(NSString*)nsString;
 @end
